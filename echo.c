9f462c0631e97f88db010fa36f95a15abc3d9aa5
Anastasiia <apashkov@c4r2p3.42vienna.com>
2024-01-18 15:33:21 +0100

    working echo

--- echo.c
+++ echo.c
@@ -6,7 +6,7 @@
 /*   By: apashkov <apashkov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 17:01:28 by apashkov          #+#    #+#             */
-/*   Updated: 2024/01/18 13:52:11 by apashkov         ###   ########.fr       */
+/*   Updated: 2024/01/18 15:02:39 by apashkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static int	validate_n_flag(char *flag)
 {
 	int	i;
 
-	i = 0;
+	i = 1;
 	if (flag[0] == '-')
 	{
 		if (!flag[1])
@@ -40,27 +40,41 @@ int echo(char *cmd[], t_alloc *mllcd)
 {
     int i;
 
-    i = 2;
+	if (!validate_n_flag(cmd[1]))
+    	i = 2;
+	else
+		i = 1;
     if (!cmd[2] && !cmd[2][0])
         write(1, "\n", 1);
 	else
 	{
 		while (cmd[i])
 		{
-			if (!validate_n_flag(cmd[i]) && i == 1)
-				i++;
-			ft_putstr_fd(cmd[i++], 1);
-			ft_putchar_fd(' ', 1);
+			ft_putstr_fd(cmd[i], 1);
+			if (cmd[i + 1])
+				ft_putchar_fd(' ', 1);
 			i++;
 		}
 		if (validate_n_flag(cmd[1]) == 1)
 			write(1, "\n", 1);
 	}
+	mllcd->exit_status = 0;
     return (0);
 }
 
-/*int main(int argc, char *argv[])
+/* int main(int argc, char *argv[])
 {
-    echo(argv[1], argv, argc);
+	t_alloc	*mllcd;
+	mllcd = (t_alloc *)malloc(sizeof(t_alloc));
+	mllcd->exit_status = 0;
+	argc = 0;
+	char	*cmd[5];
+	cmd[0] = argv[1];
+	cmd[1] = argv[2];
+	cmd[2] = argv[3];
+	cmd[3] = argv[4];
+	cmd[4] = NULL;
+ 	
+    echo(cmd, mllcd);
     return (0);
-}*/
+} */
