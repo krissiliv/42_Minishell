97507c5cc39e1547b0108e004b20d71d900478e7
krissiliv <pia.gober@gmx.at>
2024-01-23 21:47:29 +0100

    >>

--- outredir_appendmode.c
+++ outredir_appendmode.c
@@ -1,8 +1,30 @@
 
 #include "minishell.h"
 
-void    outputredir_appendmode(t_alloc *mllcd)
+void    redirection_appendmode(t_alloc *mllcd, char *filename)
 {
-    // if (mllcd.in_pars.cmd_table[0][4])
+    int fd;
 
+    fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (fd == -1)
+    {
+        ft_putstr_fd("Error: Output redirection iun append mode failed.\n", 2);
+        mllcd->exit_status = 1;
+        return ;
+    }
+    dup2(fd, 1);
+    close(fd);
+    return ;
+}
+
+int main(void)
+{
+    t_alloc *mllcd;
+    char *filename;
+
+    mllcd = (t_alloc *)malloc(sizeof(t_alloc));
+    mllcd->exit_status = 0;
+    filename = "_testfile";
+    redirection_appendmode(mllcd, filename);
+    return (0);
 }
