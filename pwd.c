2eb4f0bb631273d7bd8f6d18e61dd94d6c72d505
Anastasiia Pashkova <apashkov@c2r5p3.42vienna.com>
2024-04-03 16:22:32 +0200

    pwd norminetted

--- pwd.c
+++ pwd.c
@@ -6,28 +6,27 @@
 /*   By: apashkov <apashkov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 13:24:16 by pgober            #+#    #+#             */
-/*   Updated: 2024/02/06 14:34:26 by apashkov         ###   ########.fr       */
+/*   Updated: 2024/04/03 15:53:29 by apashkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// get_cwd allocates memory, so i need to make sure to free it
-
-int pwd()
+int	pwd(void)
 {
-    char    *cwd;
+	char	*cwd;
 
-    cwd = getcwd(NULL, 1024);
-    if (!cwd)
-    {
-        perror("pwd");
-        return (1);
-    }
-    ft_putendl_fd(cwd, 1);
-    free(cwd);
-    return (0);
+	cwd = getcwd(NULL, 1024);
+	if (!cwd)
+	{
+		perror("pwd");
+		return (1);
+	}
+	ft_putendl_fd(cwd, 1);
+	free(cwd);
+	return (0);
 }
+
 /*int main()
 {
     if (pwd() == 0)
