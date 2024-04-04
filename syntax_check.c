94aba166b159dc1ee00328a5d756954f19b70502
Pia Gober <pgober@c2r5p2.42vienna.com>
2024-04-04 13:01:37 +0200

    full normcorrection of syntax_check

--- syntax_check.c
+++ syntax_check.c
@@ -5,41 +5,53 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pgober <pgober@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/09 12:24:51 by pgober            #+#    #+#             */
-/*   Updated: 2024/02/09 14:09:58 by pgober           ###   ########.fr       */
+/*   Created: 2024/04/04 12:58:53 by pgober            #+#    #+#             */
+/*   Updated: 2024/04/04 12:59:23 by pgober           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "minishell.h"
+#include "minishell.h"
 
-int syntax_checker(char **m_argv, int m_argc) // syntax errror is always bash(2)
+static char	*syntax_check_message(int m)
 {
-	int i;
+	if (m == 1)
+		return ("minishell: syntax error near unexpected token.\n");
+	return (NULL);
+}
+
+int	syntax_checker(char **m_argv, int m_argc)
+{
+	int	i;
 
 	i = 0;
 	while (i < m_argc)
 	{
-        if ((ft_strcmp(m_argv[i], "<") == 0) && (!m_argv[++i] || special_operator(m_argv[i]) != -1))
-            return (ft_putstr_fd("minishell: syntax error near unexpected token.\n", 2), 2);
-		else if (ft_strcmp(m_argv[i], ">") == 0 && (!m_argv[++i] || special_operator(m_argv[i]) != -1))
-            return (ft_putstr_fd("minishell: syntax error near unexpected token.\n", 2), 2);
-		else if (ft_strncmp(m_argv[i], "<<", 2) == 0 && ft_strlen(m_argv[i]) == 2 && (!m_argv[++i] || special_operator(m_argv[i]) != -1)) // next one should be a file
-				return (ft_putstr_fd("minishell: syntax error near unexpected token.\n", 2), 2);
-		else if (ft_strcmp(m_argv[i], ">>") == 0 && (!m_argv[++i] || special_operator(m_argv[i]) != -1))
-            return (ft_putstr_fd("minishell: syntax error near unexpected token.\n", 2), 2);
-        else if (ft_strcmp(m_argv[i], "|") == 0 && (!m_argv[i + 1] || i == 0))
-            return (ft_putstr_fd("minishell: syntax error near unexpected token.\n", 2), 2);
+		if ((ft_strcmp(m_argv[i], "<") == 0) && \
+			(!m_argv[++i] || special_operator(m_argv[i]) != -1))
+			return (ft_putstr_fd(syntax_check_message(1), 2), 2);
+		else if (ft_strcmp(m_argv[i], ">") == 0 && (!m_argv[++i] || \
+			special_operator(m_argv[i]) != -1))
+			return (ft_putstr_fd(syntax_check_message(1), 2), 2);
+		else if (ft_strncmp(m_argv[i], "<<", 2) == 0 && \
+			ft_strlen(m_argv[i]) == 2 && (!m_argv[++i] || \
+				special_operator(m_argv[i]) != -1))
+			return (ft_putstr_fd(syntax_check_message(1), 2), 2);
+		else if (ft_strcmp(m_argv[i], ">>") == 0 && (!m_argv[++i] || \
+			special_operator(m_argv[i]) != -1))
+			return (ft_putstr_fd(syntax_check_message(1), 2), 2);
+		else if (ft_strcmp(m_argv[i], "|") == 0 && (!m_argv[i + 1] \
+			|| i == 0))
+			return (ft_putstr_fd(syntax_check_message(1), 2), 2);
 		i++;
 	}
-	return (0);  // next step: 
+	return (0);
 }
 
-int  pre_check_input(char *input_str)
+int	pre_check_input(char *input_str)
 {
-	int i;
-	bool check_only_spaces_ops;
+	int		i;
+	bool	check_only_spaces_ops;
 
-	// printf("input_str: %s has length %zu\n", input_str, ft_strlen(input_str));
 	if (!input_str || ft_strlen(input_str) == 0)
 	{
 		printf("\n");
